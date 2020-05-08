c250f2bc1b5e47459e06e860f28e6e4fb756bf8c
Sebas Belaustegui <sebasbelaustegui@hotmail.com>
2020-05-08 19:02:50 -0300

    fest mode added bug fix

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,10 +8,30 @@ import {timeout} from "rxjs/operators";
 })
 export class Tab1Page implements OnInit{
 
+  festMode = false;
+  headphonesItems = [
+    'Conectá tus auriculares',
+    'Subí el volumen',
+    'Aumentá el brillo de tu pantalla',
+    'Apagá las luces',
+    'Cerrá los ojos',
+    'Acercá la pantalla de tu teléfono a tus ojos',
+    'Disfrutá la Experiencia Boliche!'
+  ];
+  festItems = [
+    'Subí el volumen de tu dispositivo',
+    'Aumentá el brillo de tu pantalla',
+    'Apagá las luces',
+    'Reproduce una canción desde "Experiencia"',
+    'Apoyá tu teléfono en una mesa',
+    'Disfrutá la Experiencia Boliche!'
+  ];
+
   constructor() {
   }
 
   ngOnInit(): void {
+
     // Check dark mode
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
     this.toggleDarkTheme(prefersDark.matches);
@@ -22,6 +42,10 @@ export class Tab1Page implements OnInit{
 
   toggleDarkTheme(shouldAdd) {
     document.body.classList.toggle('dark', shouldAdd);
+  }
+
+  festModeToggle(){
+    this.festMode = !this.festMode;
   }
 
 }
